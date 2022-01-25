98a05a7b3f26dcd85456cd10a470844de24b06e7
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-01-25 20:36:27 +0000

    Todo

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Form } from "react-bootstrap";
+
+const TodoList = (props) => {
+	return (
+		<>
+			<ul className="list-group list-group-flush container">
+				<li className="list-group-item"></li>
+				<li className="list-group-item d-flex bd-highlight">
+					<div className="p-2 flex-grow-1 bd-highlight">
+						{props.todo}
+					</div>
+					<Form.Check
+						className="checkbox p-2 bd-highlight"
+						onChange={props.checked}
+						type={"checkbox"}
+					/>
+					<i
+						className="far fa-trash-alt p-2 bd-highlight my-1"
+						onClick={() => props.delete(props.id)}></i>
+				</li>
+			</ul>
+		</>
+	);
+};
+
+TodoList.propTypes = {
+	todo: PropTypes.string,
+	delete: PropTypes.func,
+	id: PropTypes.number,
+};
+
+export default TodoList;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//Import Components
+import TodoList from "./TodoList.jsx";
 
 //create your first component
 const Home = () => {
+	const [list, setList] = useState([]);
+	const [todo, setTodo] = useState({
+		done: false,
+	});
+	const [check, setCheck] = useState(false);
+
+	console.log(list);
+	console.log({ todo });
+
+	const handleClick = () => {
+		const newList = [...list, todo];
+		setList(newList);
+	};
+
+	const deleteTodo = (id) => {
+		const listDelete = [...list];
+		listDelete.splice(id, 1);
+		setList(listDelete);
+	};
+
+	const checkTodo = () => {};
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card container d-flex justify-content-center col-6">
+			<div className="card-body row">
+				<div className="col-12 d-flex justify-content-center">
+					<h5 className="card-title">TO DO LIST</h5>
+				</div>
+				<div className="col-12 d-flex justify-content-center">
+					<p className="card-text row">
+						Add the tasks to create your list
+					</p>
+				</div>
+				<InputGroup
+					id="new-todo"
+					onChange={(e) => {
+						setTodo(e.target.value);
+					}}
+					size="lg">
+					<FormControl
+						aria-label="Large"
+						aria-describedby="inputGroup-sizing-sm"
+					/>
+				</InputGroup>
+
+				<div className="d-flex justify-content-center my-2">
+					<Button
+						className="col-4"
+						onClick={handleClick}
+						variant="primary">
+						New Task
+					</Button>
+				</div>
+				{list.map((todo, index) => {
+					return (
+						<TodoList
+							key={index}
+							id={index}
+							todo={todo}
+							delete={deleteTodo}
+							check={checkTodo}
+						/>
+					);
+				})}
+			</div>
+
+			<div>
+				<span className="badge bg-light text-dark me-2">
+					Total Tasks: {list.length}
+				</span>
+				<span className="badge bg-success text-dark me-2">
+					Complete Tasks:
+				</span>
+			</div>
 		</div>
 	);
 };
