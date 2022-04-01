5bead1f8b9b235eb6413c5955357d26c30e57d75
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-04-01 19:08:53 +0000

    feat: priority

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,17 +1,40 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Form } from "react-bootstrap";
 
 const TodoList = (props) => {
-	console.log(props);
 	return (
 		<>
 			<ul className="list-group list-group-flush container">
-				<li className="list-group-item"></li>
 				<li className="list-group-item d-flex bd-highlight">
 					<div className="p-2 flex-grow-1 bd-highlight">
 						{props.todo}
 					</div>
+					<div className="badge bg-success text-dark my-2 me-2">
+						<input
+							type="radio"
+							value="normal"
+							name="priority"
+							onChange={() => props.handle(props.id)}
+						/>
+					</div>
+
+					<div className="badge bg-warning text-dark my-2 me-2">
+						<input
+							type="radio"
+							value="important"
+							name="priority"
+							onChange={() => props.handle(props.id)}
+						/>
+					</div>
+					<div className="badge bg-danger text-dark my-2 me-2">
+						<input
+							type="radio"
+							value="urgent"
+							name="priority"
+							onChange={() => props.handle(props.id)}
+						/>
+					</div>
+
 					<i
 						className="far fa-trash-alt p-2 bd-highlight my-1"
 						onClick={() => props.delete(props.id)}></i>
@@ -22,9 +45,10 @@ const TodoList = (props) => {
 };
 
 TodoList.propTypes = {
-	todo: PropTypes.object,
+	todo: PropTypes.string,
 	delete: PropTypes.func,
 	id: PropTypes.number,
+	handle: PropTypes.func,
 };
 
 export default TodoList;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,30 +13,10 @@ let countUrgent = 0;
 //create your first component
 const Home = () => {
 	const [list, setList] = useState([]);
-	const [todo, setTodo] = useState({});
-
-	const modifyPriority = (todo, oldPriority, newPriority, id) => {
-		if (newPriority == "normal") {
-			countNormal++;
-		} else if (newPriority == "importante") {
-			countImportant++;
-		} else {
-			countUrgent++;
-		}
-		if (oldPriority == "normal") {
-			countNormal--;
-		} else if (oldPriority == "importante") {
-			countImportant--;
-		} else {
-			countUrgent--;
-		}
-
-		const newList = list.filter(function (todo, index) {
-			return index != id;
-		});
-		const newListTodo = [...newList, { todo: todo, priority: newPriority }];
-		setList(newListTodo);
-	};
+	const [todo, setTodo] = useState({
+		task: "",
+		priority: "",
+	});
 
 	const listTodo = () => {
 		return list.map((todo, index) => {
@@ -46,36 +26,23 @@ const Home = () => {
 					id={index}
 					todo={todo}
 					delete={deleteTodo}
-					modifyPriority={modifyPriority}
+					handle={handleChangeRadio}
 				/>
 			);
 		});
 	};
 
 	console.log(list);
-	console.log({ todo });
+	console.log(todo);
 
 	const handleClick = () => {
 		const newList = [...list, todo];
 		setList(newList);
-		if (todo.priority == "normal") {
-			countNormal++;
-		} else if (todo.priority == "importante") {
-			countImportant++;
-		} else {
-			countUrgent++;
-		}
 	};
 
-	const deleteTodo = (id) => {
-		if (listTodo[id].priority == "normal") {
-			countNormal--;
-		} else if (listTodo[id].priority == "importante") {
-			countImportant--;
-		} else {
-			countUrgent--;
-		}
+	const handleChangeRadio = () => {};
 
+	const deleteTodo = (id) => {
 		const listDelete = [...list];
 		listDelete.splice(id, 1);
 		setList(listDelete);
@@ -103,7 +70,6 @@ const Home = () => {
 						aria-describedby="inputGroup-sizing-sm"
 					/>
 				</InputGroup>
-
 				<div className="d-flex justify-content-center my-2">
 					<Button
 						className="col-4"
@@ -112,25 +78,6 @@ const Home = () => {
 						New Task
 					</Button>
 				</div>
-				<select
-					className="form-select bg-light"
-					aria-label="Priority"
-					defaultValue="Select Priority"
-					onChange={(event) => {
-						priority = event.target.value;
-						setTodo({ todo: task, priority: priority });
-					}}>
-					<option>Select priority</option>
-					<option value="normal" className="bg-success">
-						Normal
-					</option>
-					<option value="urgent" className="bg-danger">
-						Urgent
-					</option>
-					<option value="important" className="bg-warning">
-						Important
-					</option>
-				</select>
 			</div>
 			{!list.length ? (
 				<div className="alert alert-info border-2" role="alert">
@@ -144,7 +91,13 @@ const Home = () => {
 					Total Tasks: {list.length}
 				</span>
 				<span className="badge bg-success text-dark me-2">
-					Complete Tasks: {list.ck}
+					Normal Tasks: {}
+				</span>
+				<span className="badge bg-warning text-dark me-2">
+					Important Tasks: {}
+				</span>
+				<span className="badge bg-danger text-dark me-2">
+					Urgent Tasks: {}
 				</span>
 			</div>
 		</div>
